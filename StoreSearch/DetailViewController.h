ea548315043b103637aef1acb027c8cbf82418a3
Sheng-Loong Su <sushengloong@gmail.com>
2013-07-28 22:05:29 +0800

    use containment api to display details view

---
+++ StoreSearch/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  StoreSearch
+//
+//  Created by Su Sheng Loong on 7/28/13.
+//  Copyright (c) 2013 Su Sheng Loong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ StoreSearch/DetailViewController.m
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.m
+//  StoreSearch
+//
+//  Created by Su Sheng Loong on 7/28/13.
+//  Copyright (c) 2013 Su Sheng Loong. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    NSLog(@"dealloc %@", self);
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)close:(id)sender
+{
+    [self willMoveToParentViewController:nil];
+    [self.view removeFromSuperview];
+    [self removeFromParentViewController];
+}
+
+@end
