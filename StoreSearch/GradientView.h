9854bef8dba8a6bbf77afe23b6e76c01afda29d1
Sheng-Loong Su <sushengloong@gmail.com>
2013-07-29 12:36:40 +0800

    add gradient view as detail popup background

---
+++ StoreSearch/GradientView.h
@@ -0,0 +1,13 @@
+//
+//  GradientView.h
+//  StoreSearch
+//
+//  Created by Su Sheng Loong on 7/29/13.
+//  Copyright (c) 2013 Su Sheng Loong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GradientView : UIView
+
+@end
