61818e558599a8dd5d47620246780edba2c2d396
Sheng-Loong Su <sushengloong@gmail.com>
2013-07-29 12:39:13 +0800

    make search bar resign first responder after selecting a row

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -98,6 +98,7 @@ static NSString *const LoadingCellIdentifier = @"LoadingCell";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    [self.searchBar resignFirstResponder];
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
     DetailViewController *controller = [[DetailViewController alloc] initWithNibName:@"DetailViewController" bundle:nil];
